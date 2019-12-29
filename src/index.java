0249777aca33526ed563aa6c9df12ad339ef8f65
maindsamiksha <58458482+maindsamiksha@users.noreply.github.com>
2019-12-29 23:07:29 +0530

    Update index.java

--- src/index.java
+++ src/index.java
@@ -26,6 +26,7 @@ public class index extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("sami");
 	}
 
 	/**
